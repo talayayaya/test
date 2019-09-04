68b8558f51c74ef708875b92a2b011a9eec877a3
laya <tangyh@nettl.cn>
2019-09-04 18:25:54 +0800

    1111

---
+++ module2/src/main/java/com/test1/MyService2.java
@@ -0,0 +1,28 @@
+package com.test1;
+
+
+public class MyService2 {
+
+
+    public void att(){
+        System.out.println("test");
+    }
+
+    public void aaa(){
+        System.out.println("test");
+
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+
+    public void aaa111(){
+        System.out.println("test");
+
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
